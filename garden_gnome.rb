3d23054f428edfc46183b1dbd572da9b0bd30b66
chloehaynes <19chaynes@nerinxhs.org>
2018-06-21 20:43:16 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -5,8 +5,8 @@ class GardenGnome
   attr_accessor :name, :age, :gluten_allergy
   
   def initialize(personality,hat_color)
-    @personality = "evil"
-    @hat_color = "red"
+    @personality = personality
+    @hat_color = hat_color
   end
   
   def sethat_color=(hat_color)
