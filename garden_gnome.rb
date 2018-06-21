288d503571525a6235eed1322c6fb012f509b752
chloehaynes <19chaynes@nerinxhs.org>
2018-06-21 20:32:02 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -2,6 +2,13 @@
 
 
 class GardenGnome
+  attr_accessor :name, :age, :gluten_allergy
+  
+  def initialize(personality,hat_color)
+    @personality = "evil"
+    @hat_color = "red"
+  end
+  
   
   
 end
