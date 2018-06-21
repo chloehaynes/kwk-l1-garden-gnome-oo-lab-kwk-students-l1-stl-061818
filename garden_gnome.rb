9efd7e256540df894eb860b502aa2c698b3968f6
chloehaynes <19chaynes@nerinxhs.org>
2018-06-21 20:44:28 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -31,8 +31,8 @@ class GardenGnome
   
 end
 
-gnome1 = GardenGnome.new
-gnome2 = GardenGnome.new("Walter the Worst")
+gnome1 = GardenGnome.new("gnome", 14)
+gnome2 = GardenGnome.new("Walter the Worst", 78)
 gnome3 = GardenGnome.new("James the Jerk", 3421)
 gnome4 = GardenGnome.new("Alfred the Abhorrent", 579, "true")
 
